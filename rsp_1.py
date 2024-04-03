fa8a7bcf0efd7193a382bdf7939fea8395e3cd54
zbz95 <85227665+zbz95@users.noreply.github.com>
2024-04-03 09:16:05 +0000

    Add files via upload

---
+++ rsp_1.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+def count_rsp(df_clean, mrp = 3000, malyi_sotr = 100, malyi_dohod = 300000, 
+              micro_sotr = 15, micro_dohod = 30000,
+              krupnyi_sotr = 250, krupnyi_dohod = 3000000):
+    
+    df_clean['СГД'] = df_clean['СГД'].astype(np.float64)
+    df_clean['СГЧС'] = df_clean['СГЧС'].astype(np.float64)
+    
+    krupnyi = df_clean.loc[(df_clean['СГЧС']>krupnyi_sotr) | (df_clean['СГД']>krupnyi_dohod*mrp)]
+    malyi = df_clean.loc[(df_clean['СГЧС']<=malyi_sotr) & (df_clean['СГД']<=malyi_dohod*mrp)]
+    
+    micro = malyi.loc[(malyi['СГЧС']<=micro_sotr) | (malyi['СГД']<=micro_dohod*mrp)]
+    
+    print(f'Субъектов крупного предпринимательства = {len(krupnyi)}')
+    print(f'Субъектов среднего предпринимательства = {len(df_clean) - len(krupnyi) - len(malyi)}')
+    print(f'Субъектов малого предпринимательства = {len(malyi)}, из них субъектов микро предпринимательства {len(micro)}')
