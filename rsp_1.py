032fe84934fa0c37a730d0e983eab999cdd1c3b7
zbz95 <85227665+zbz95@users.noreply.github.com>
2024-04-05 09:53:39 +0000

    Add files via upload

--- rsp_1.py
+++ rsp_1.py
@@ -16,3 +16,33 @@ def count_rsp(df_clean, mrp = 3000, malyi_sotr = 100, malyi_dohod = 300000,
     print(f'Субъектов крупного предпринимательства = {len(krupnyi)}')
     print(f'Субъектов среднего предпринимательства = {len(df_clean) - len(krupnyi) - len(malyi)}')
     print(f'Субъектов малого предпринимательства = {len(malyi)}, из них субъектов микро предпринимательства {len(micro)}')
+    
+    
+def count_rsp_sotr(df_clean, malyi_sotr = 100, 
+              micro_sotr = 15, krupnyi_sotr = 250):
+    
+    df_clean['СГЧС'] = df_clean['СГЧС'].astype(np.float64)
+    
+    krupnyi = df_clean.loc[(df_clean['СГЧС']>krupnyi_sotr)]
+    malyi = df_clean.loc[(df_clean['СГЧС']<=malyi_sotr)]
+    
+    micro = malyi.loc[(malyi['СГЧС']<=micro_sotr)]
+    
+    print(f'Субъектов крупного предпринимательства = {len(krupnyi)}')
+    print(f'Субъектов среднего предпринимательства = {len(df_clean) - len(krupnyi) - len(malyi)}')
+    print(f'Субъектов малого предпринимательства = {len(malyi)}, из них субъектов микро предпринимательства {len(micro)}')
+    
+    
+def count_rsp_dohod(df_clean, mrp = 3000,  malyi_dohod = 300000, 
+                    micro_dohod = 30000, krupnyi_dohod = 3000000):
+    
+    df_clean['СГД'] = df_clean['СГД'].astype(np.float64)
+    
+    krupnyi = df_clean.loc[(df_clean['СГД']>krupnyi_dohod*mrp)]
+    malyi = df_clean.loc[(df_clean['СГД']<=malyi_dohod*mrp)]
+    
+    micro = malyi.loc[(malyi['СГД']<=micro_dohod*mrp)]
+    
+    print(f'Субъектов крупного предпринимательства = {len(krupnyi)}')
+    print(f'Субъектов среднего предпринимательства = {len(df_clean) - len(krupnyi) - len(malyi)}')
+    print(f'Субъектов малого предпринимательства = {len(malyi)}, из них субъектов микро предпринимательства {len(micro)}')
